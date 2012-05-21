db63c1cbbcb98268b6606f9bcf0e7b54a2a2613a
Michael Elsdörfer <michael@elsdoerfer.com>
2012-05-21 16:00:56 +0200

    Added some alternate keyboard shortcuts.
    Some of those are useful when using mouse mode.

--- app/js/shortcuts.js
+++ app/js/shortcuts.js
@@ -10,35 +10,39 @@
 Shortcuts = function(app) {
     var audio = app.audio;
     $(document).on('keydown', function(e) {
-        if (e.keyCode == 38) { // up
+        var key = e.keyCode;
+
+        if (key == 38) { // up
             app.setPlaybackRate('+0.1');
             return false;
         }
-        else if (e.keyCode == 40) {  // down
+        else if (key == 40) {  // down
             app.setPlaybackRate('-0.1');
             return false;
         }
-        else if (e.keyCode == 32) {  // space
+        else if (key == 32 || key == 13) { // space or return
             if (audio.paused) {
                 audio.play();
                 return false;
             }
-            else if (e.ctrlKey) {
+            else if (key == 13 || e.ctrlKey) {  // ctrl+space or return
                 audio.pause();
                 return false;
             }
         }
-        else if (e.keyCode == 37) {  // left
-            if (e.ctrlKey) {
-                audio.currentTime -= 1;
-                return false;
-            }
+        else if (
+                (e.ctrlKey && key == 37) ||     // left
+                key == '65' ||                  // a
+                key == 100) {                   // numpad 4
+            audio.currentTime -= 1;
+            return false;
         }
-        else if (e.keyCode == 39) {  // right
-            if (e.ctrlKey) {
-                audio.currentTime += 1;
-                return false;
-            }
+        else if (
+                (e.ctrlKey && key == 39) ||     // right
+                key == 68 ||                    // d
+                key == 102) {                   // numpad 6
+            audio.currentTime += 1;
+            return false;
         }
     });
 }
